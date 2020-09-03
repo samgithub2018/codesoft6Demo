304f9d787ca938ab7594eb46d0cf444c69c80a94
sam <mogash@163.com>
2020-09-03 23:13:48 +0800

    first commit

---
+++ TestCodeSoft/Main.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TestCodeSoft
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(187, 193);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(99, 34);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "列印";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 258);
+            this.Controls.Add(this.btnPrint);
+            this.Name = "Main";
+            this.Text = "列印界面";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ TestCodeSoft/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using LabelManager2;
+using System.IO;
+
+namespace TestCodeSoft
+{
+    public partial class Main : Form
+    {
+
+        private int ilablelx;//定义列印类型
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            LabelManager2.Application labelapp = null;
+            LabelManager2.Document labDoc = null;
+            string filePathName = System.Windows.Forms.Application.StartupPath + "\\Document1.lab";
+            if (!File.Exists(filePathName))
+            {
+                MessageBox.Show("请准备好列印模板");
+                return;
+            }
+            try
+            {
+                labelapp = new LabelManager2.Application();
+                labelapp.Documents.Open(filePathName, false);//调用设计好的模板
+                labDoc = labelapp.ActiveDocument;
+                labDoc.Variables.FormVariables.Item("Date").Value = System.DateTime.Now.ToString("yyyy-MM-dd");//往变量里面写入数据
+                labDoc.PrintDocument(1);//列印份数
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                labelapp.Documents.CloseAll(true);
+                labelapp.Quit();
+                labelapp = null;
+                labDoc = null;
+            }
+
+        }
+    }
+}
